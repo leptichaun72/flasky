e46b556be91201ba253ce29ee66b7a1d849992d9
iamgayasfuck <leptichaun72@github.com>
2020-07-10 02:06:06 -0400

    Add more functionalities

--- model.py
+++ model.py
@@ -1,23 +1,26 @@
+##############
+# DATABASE
+##############
 import os
-
+import random
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "proj.db"))
 
-doodaadee = 10002
-
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,\
+    render_template,\
+    request,\
+    redirect,\
+    url_for,\
+    make_response,\
+    send_file,\
+    jsonify
  
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
-
-class Animal(db.Model):
-	id = db.Column('id', db.Integer, primary_key=True)
-	data = db.Column('data', db.Unicode)
-
 
 class Example(db.Model):
 	__tablename__ = 'exampledude'
@@ -36,15 +39,82 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-# a route
+########
+# APP
+########
 @app.route('/')
+def index():
+    return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
 
-# the controller action
-def main_page():
-    pass # used as a placeholder
-    print(f'mah date: {doodaadee}')
+@app.route('/login', methods=['POST','GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('dashboard',name = user))
+   else:
+      user = request.args.get('name')
+      print('user type: {}'.format(user)) 
+      return render_template('login.html')
+
+@app.route('/dashboard/<name>')
+def dashboard(name):
+   return 'welcome %s' % name
+
+#########
+# OTHER
+#########
+@app.route('/main')  # a route
+def main_page(): # the controller action
     tasks = Todo.query.order_by(Todo.date_created).all()
-    return render_template('index.html', my_tasks=tasks)
+    return render_template('model.html', my_tasks=tasks)
+
+@app.route('/users/<name>', methods=['POST'])
+def create_user(name):
+    msg = f'user {name} created'
+    return make_response(msg, 205)
+
+@app.route('/users/<name>', methods=['GET'])
+def get_user(name):
+    msg = f'Hello {name}'
+    return make_response(msg, 200)
+
+@app.route('/image')
+def get_image():
+    filename = 'airplane-sunset.jpg'
+    return send_file(filename, mimetype='image/jpg')
+
+movies = {
+    1: 'Toy Story',
+    2: 'Star Wars',
+    3: 'Kung Fu Panda',
+    4: 'The Lion King',
+    5: 'Ip Man'
+}
+
+@app.route('/movies')
+def get_movies():
+    return movies
+
+# converting tuple to json through jsonify func
+@app.route('/movie/random')
+def random_movie():
+    # returns a tuple e.g. (5, 'Ip Man')
+    movie = random.choice(list(movies.items()))
+    return jsonify(movie)
+
+@app.route('/stache/<animal>')
+def listen_animal(animal):
+    sound = f'Hear a {animal} roar!'
+    print(type(sound))
+    return render_template('roar.html', creature=sound)
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.errorhandler(404)
+def cant_find_page(err):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
